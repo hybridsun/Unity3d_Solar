92d6fe66bfb3af6d02f7394a831c61e68d539091
hybridSun <86775511+hybridsun@users.noreply.github.com>
2022-07-26 03:42:32 +0530

    Add files via upload

---
+++ Assets/Scripts/Rings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rings : MonoBehaviour 
+{
+	/// <summary>
+	/// The "planet" object that this "rings" object is associated with.
+	/// </summary>
+	public GameObject planet;
+
+	void LateUpdate () 
+	{
+		transform.position = planet.transform.position;
+	}
+}
